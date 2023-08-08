0166a29d6b83810bb7117ae5196df68233c09dac
Sethum Vidmal <sethumthabrew@gmail.com>
2023-08-08 22:18:41 +0530

    UI improvements and new clear button

--- src/Window.java
+++ src/Window.java
@@ -2,29 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 public class Window extends JFrame implements Observer{
     private final JButton btnSend = new JButton();
+    private final JButton btnClear = new JButton();
     private final JTextField message = new JTextField();
     private  final JTextArea display = new JTextArea();
     private final String title;
     public Window(String title){
         this.title = title;
         setTitle(title);
-        setSize(400,400);
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize(400,350);
+        setLayout(null);
         setLocationRelativeTo(null);
-        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
         display.setEditable(false);
+        display.setFont(new Font("",1,15));
+        display.setBounds(15, 60, 360, 200);
+        add(display);
+
         btnSend.setText("Send");
         btnSend.setFont(new Font("",1,20));
-        display.setFont(new Font("",1,15));
-        message.setFont(new Font("",1,12));
+        btnSend.setBounds(220, 265, 90, 40);
         btnSend.addActionListener((evt)->{
             Observable.getInstance().getMessage(title,message.getText());
             message.setText("");
         });
-        add("South",btnSend);
-        add("North",message);
-        add("Center",display);
+        add(btnSend);
+
+        message.setFont(new Font("",1,12));
+        message.setBounds(15, 10, 360, 35);
+        add(message);
+
+        btnClear.setText("Clear");
+        btnClear.setFont(new Font("",1,20));
+        btnClear.setBounds(120, 265, 90, 40);
+        btnClear.addActionListener((evt)->{
+            display.setText("");
+        });
+        add(btnClear);
+
+        setVisible(true);
     }
     @Override
     public void setMessage(String senderName, String message) {
@@ -33,9 +49,5 @@ public class Window extends JFrame implements Observer{
         }else {
             display.append(senderName + " : " + message + "\n");
         }
-    }
-    @Override
-    public String getTitle() {
-        return title;
     }
 }
